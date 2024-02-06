f8fa92dd55503b55661c3e14cb727262cd0370a9
Ugochi Ukaegbu <ugochiukaegbu19@gmail.com>
2024-02-06 14:18:07 +0100

    file used to deploy resources in the prod environment

--- production/root-modules.tf
+++ production/root-modules.tf
@@ -1,27 +1,27 @@
 
 module "EC2_Instance" {
-  source  = "../child-modules/EC2-Instance"
+  source                           = "../child-modules/EC2-Instance"
   
- instance_name = "VM-2"
- instance_ami = "ami-06dd92ecc74fdfb36"
- ec2_instance_type = "t2.micro"
- key_name = "mykeypair"
- key_filename = "/home/ubuntu/.ssh/id_rsa"
- security-name = "security-central"
- vpc_id_id = module.network.vpc_id_id
- private_subnet_id = module.network.private_subnet_id
- public_subnet_id = module.network.public_subnet_id
- environment = "prod"
+ instance_name                     = var.instance_name
+ instance_ami                      = var.instance_ami
+ ec2_instance_type                 = var.ec2_instance_type
+ key_name                          = var.key_name
+ key_filename                      = var.key_filename
+ security-name                     = var.security-name
+ vpc_id_id                         = module.network.vpc_id_id
+ private_subnet_id                 = module.network.private_subnet_id
+ public_subnet_id                  = module.network.public_subnet_id
+ environment                       = var.environment
 }
  
 
 
 
 module "network" {
-  source = "../child-modules/network"
-  environment = "prod"
-  cidr_block = "10.58.0.0/16"
-  vpc_name = "VPC-Central"
-  public_subnet = "10.58.1.0/24"
-  private_subnet = "10.58.2.0/24"
+  source                           = "../child-modules/network"
+  environment                      = var.environment
+  cidr_block                       = var.cidr_block
+  vpc_name                         = var.vpc_name
+  public_subnet                    = var.public_subnet
+  private_subnet                   = var.private_subnet
 }
