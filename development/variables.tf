08afa3db696b9a8e2ac6b63b8afb5b6a2669cab9
Ugochi Ukaegbu <ugochiukaegbu19@gmail.com>
2024-02-06 14:59:46 +0100

    files not uploaded

---
+++ development/variables.tf
@@ -0,0 +1,11 @@
+variable "instance_name" {}
+variable "instance_ami" {}
+variable "ec2_instance_type" {}
+variable "key_name" {}
+variable "key_filename" {}
+variable "security-name" {} 
+variable "environment" {}
+variable "cidr_block" {}
+variable "public_subnet" {}
+variable "private_subnet" {}
+variable "vpc_name" {}
